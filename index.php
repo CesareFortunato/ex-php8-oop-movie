9b460d97820f38dc3d278544a4166151f1bdf500
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-08 13:02:25 +0200

    feat: add genre

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <?php
 
+class Genre {
+    public $genre;
+    public $subGenre;
+    public function __construct($_genre, $_subGenre){
+        $this->genre = $_genre;
+        $this->subGenre = $_subGenre;
+    }
+}
+
 class Movie
 {
     public $title;
     public $poster;
 
     public $vote;
+    public $genre;
 
-    function __construct($_title, $_poster, $_vote)
+    function __construct($_title, $_poster, $_vote, Genre $_genre)
     {
         $this->vote = $_vote;
         $this->poster = $_poster;
         $this->title = $_title;
+        $this->genre = $_genre;
     }
 
     public function getTitle(){
         return $this->title;
     }
 }
+
+
 
 ?>
 
@@ -36,21 +49,23 @@ class Movie
 
 
     <?php
-    $lotr = new Movie("il Signore degli anelli", "https://www.ciakclub.it/wp-content/uploads/2023/02/il-signore-degli-anelli-la-compagnia-dell-anello-poster-768x1153.jpg", 5);
-    $bladeRunner = new Movie("Blade Runner", "https://tse4.mm.bing.net/th/id/OIP.UW5IFNWOtG55kH1mdE4jhwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3", 5);
+    $lotr = new Movie("il Signore degli anelli", "https://www.ciakclub.it/wp-content/uploads/2023/02/il-signore-degli-anelli-la-compagnia-dell-anello-poster-768x1153.jpg", 5, new Genre("Fantasy", "Epic"));
+    $bladeRunner = new Movie("Blade Runner", "https://tse4.mm.bing.net/th/id/OIP.UW5IFNWOtG55kH1mdE4jhwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3", 5, new Genre("sci-fi", "mistery"));
     ?>
     <div class="card" style="width: 18rem;">
         <img src="<?php echo $lotr->poster ?>" class="card-img-top" alt="<?php echo $lotr->title ?>">
         <div class="card-body">
             <h5 class="card-title"><?php echo $lotr->title ?></h5>
-            <p class="card-text">Vote: <?php echo $lotr->vote ?></p>
+            <p class="card-text">Genere: <?php echo $lotr->genre->genre ?> / <?php echo $lotr->genre->subGenre ?></p>
+            <p class="card-text">Voto: <?php echo $lotr->vote ?></p>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="<?php echo $bladeRunner->poster ?>" class="card-img-top" alt="<?php echo $bladeRunner->title ?>">
         <div class="card-body">
             <h5 class="card-title"><?php echo $bladeRunner->title ?></h5>
-            <p class="card-text">Vote: <?php echo $bladeRunner->vote ?></p>
+            <p class="card-text">Genere: <?php echo $bladeRunner->genre->genre ?> / <?php echo $bladeRunner->genre->subGenre ?></p>
+            <p class="card-text">Voto: <?php echo $bladeRunner->vote ?></p>
         </div>
     </div>
 </body>
